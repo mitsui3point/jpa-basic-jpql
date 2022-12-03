2f168ec83dfa6ca5b5793cb9725e0c9dac43e029
mitsui3point <rladbsgh0331@gmail.com>
2022-12-03 17:54:53 +0900

    feat($jpaMain, $entity): update - update 집합과 정렬 • GROUP BY, HAVING • ORDER BY

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,9 +1,10 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import jpql.entity.Member;
+
+import javax.persistence.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,6 +16,53 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member1 = new Member();
+            member1.setUsername("user1");
+            member1.setAge(11);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("user2");
+            member2.setAge(13);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("user2");
+            member3.setAge(22);
+            em.persist(member3);
+
+            Member member4 = new Member();
+            member4.setUsername("user3");
+            member4.setAge(29);
+            em.persist(member4);
+
+            Member member5 = new Member();
+            member5.setUsername("user1");
+            member5.setAge(29);
+            em.persist(member5);
+
+            List<Object[]> members = em.createQuery(
+                    "select m.username," +
+                            " count(m) as cnt," +
+                            " sum(m.age) as sum," +
+                            " avg(m.age) as avg," +
+                            " max(m.age) as max," +
+                            " min(m.age) as min" +
+                            " from Member as m" +
+                            " group by m.username" +
+                            " having count(m) > 1" +
+                            " order by m.username desc")
+                    .getResultList();
+
+            for (Object[] member : members) {
+                System.out.println("========================");
+                System.out.println("member = " + member[0]);
+                System.out.println("member = " + member[1]);
+                System.out.println("member = " + member[2]);
+                System.out.println("member = " + member[3]);
+                System.out.println("member = " + member[4]);
+                System.out.println("member = " + member[5]);
+            }
 
             tx.commit();
         } catch (Exception e) {
