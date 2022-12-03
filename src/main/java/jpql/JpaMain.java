669d5c768eefcdfbed00c002d4ac78bc8ee77896
mitsui3point <rladbsgh0331@gmail.com>
2022-12-03 18:19:15 +0900

    feat($jpaMain, $entity): update - update 결과 조회 API • query.getResultList(): 결과가 하나 이상일 때, 리스트 반환 • 결과가 없으면 빈 리스트 반환

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -4,7 +4,6 @@ import jpql.entity.Member;
 
 import javax.persistence.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -21,28 +20,17 @@ public class JpaMain {
             member1.setAge(10);
             em.persist(member1);
 
-            //TypeQuery: 반환 타입이 명확할 때 사용
-            TypedQuery<Member> typedQuery1 = em.createQuery("select m from Member m", Member.class);
-            List<Member> typedQuery1Members = typedQuery1.getResultList();
-            for (Member typedQuery1Member : typedQuery1Members) {
-                System.out.println("typedQuery1Member = " + typedQuery1Member.getUsername());
+            Member member2 = new Member();
+            member2.setUsername("user2");
+            member2.setAge(12);
+            em.persist(member2);
+
+            TypedQuery<Member> typedQuery = em.createQuery("select m from Member m", Member.class);
+            List<Member> members = typedQuery.getResultList();
+            for (Member member : members) {
+                System.out.println("member = " + member.getUsername());
             }
-            
-            TypedQuery<String> typedQuery2 = em.createQuery("select m.username from Member m", String.class);
-            List<String> typedQuery2Members = typedQuery2.getResultList();
-            for (String typedQuery2Member : typedQuery2Members) {
-                System.out.println("typedQuery2Member = " + typedQuery2Member);
-            }
-            
-            //Query: 반환 타입이 명확하지 않을 때 사용
-            Query query = em.createQuery("select m.username, m.age from Member m");
-            List<Object[]> queryMembers = query.getResultList();
-            for (Object[] queryMember : queryMembers) {
-                for (Object o : queryMember) {
-                    System.out.println("o = " + o);
-                }
-            }
-            
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
