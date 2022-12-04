57dac52c921aa56933ee20e3f515012979a0ab80
mitsui3point <rladbsgh0331@gmail.com>
2022-12-05 03:31:36 +0900

    feat($jpaMain, $entity): update - update 조건식 - CASE 식 기본 CASE 식단순 CASE 식 • COALESCE: 하나씩 조회해서 null이 아니면 반환• NULLIF: 두 값이 같으면 null 반환, 다르면 첫번째 값 반환

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import jpql.entity.Member;
+import jpql.entity.Team;
 import jpql.generic.GenericPrint;
 
 import javax.persistence.EntityManager;
@@ -9,7 +10,8 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
 
-import static jpql.enumulate.MemberType.*;
+import static jpql.enumulate.MemberType.ADMIN;
+import static jpql.enumulate.MemberType.USER;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -21,17 +23,46 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = Member.createMember("username", 12, ADMIN);
+            Member member1 = Member.createMember("dd", 70, ADMIN);
+            Member member2 = Member.createMember("cc", 9, ADMIN);
+            Member member3 = Member.createMember(null, 20, USER);
+            Member member4 = Member.createMember("관리자", 20, USER);
+
+            Team teamA = Team.createTeam("teamA");
+            Team teamB = Team.createTeam("teamB");
+            Team teamC = Team.createTeam("teamC");
+
+            member1.changeTeam(teamA);
+            member2.changeTeam(teamB);
+            member3.changeTeam(teamA);
+            member4.changeTeam(teamC);
+
+            em.persist(teamA);
+            em.persist(teamB);
+            em.persist(teamC);
+
             em.persist(member1);
-            Member member2 = Member.createMember(null, 12, ADMIN);
             em.persist(member2);
+            em.persist(member3);
+            em.persist(member4);
 
             em.flush();
             em.clear();
 
-            String query ="select m " +
-                    "from Member m " +
-                    "where m.username is null ";
+            String query = "select m, " +
+                    //기본 case
+                    "case when m.age >= 60 then '경로요금' " +
+                    "     when m.age <= 10 then '학생요금' " +
+                    "                      else '일반요금' end, " +
+                    //단순 case
+                    "case m.team.name when 'teamA' then '인센110' " +
+                    "                 when 'teamB' then '인센120' " +
+                    "                 else '인센105' end, " +
+                    //COALESCE: 하나씩 조회해서 null이 아니면 반환
+                    "coalesce(m.username, '이름없는회원'), " +
+                    //NULLIF: 두 값이 같으면 null 반환, 다르면 첫번째 값 반환
+                    "nullif(m.username, '관리자') " +
+                    "from Member m ";
             List resultList = em.createQuery(query)
                     .getResultList();
 
