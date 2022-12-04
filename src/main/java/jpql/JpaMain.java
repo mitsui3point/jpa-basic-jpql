c76a4771c3ce572a15c097a7d6dbd6b0f736a0a5
mitsui3point <rladbsgh0331@gmail.com>
2022-12-04 18:53:16 +0900

    feat($jpaMain, $entity): update - update 1. 조인 대상 필터링 • 예) 회원과 팀을 조인하면서, 팀 이름이 team1인 팀만 조인

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -20,40 +20,59 @@ public class JpaMain {
 
         try {
             Team team1 = Team.createTeam("team1");
-            em.persist(team1);
             Team team2 = Team.createTeam("team2");
+            Team team3 = Team.createTeam("team3");
+            em.persist(team1);
             em.persist(team2);
+            em.persist(team3);
 
             Member member1 = Member.createMember("member1", 13);
             Member member2 = Member.createMember("member2", 20);
             Member member3 = Member.createMember("member3", 4);
+            Member member4 = Member.createMember("member4", 55);
 
             member1.changeTeam(team1);
             member2.changeTeam(team1);
+            member3.changeTeam(team2);
             em.persist(member1);
             em.persist(member2);
             em.persist(member3);
+            em.persist(member4);
 
             em.flush();
             em.clear();
+            /**
+             * [ON vs WHERE]
+             * ON : JOIN 을 하기 전 필터링을 한다 (=ON 조건으로 필터링이 된 레코들간 JOIN이 이뤄진다)
+             * WHERE : JOIN 을 한 후 필터링을 한다 (=JOIN을 한 결과에서 WHERE 조건절로 필터링이 이뤄진다)
+             * https://developyo.tistory.com/121
+             */
 
-            String query = "select m, t from Member m, Team t";
-            List<Object[]> resultList = em.createQuery(query, Object[].class)
+//            String query = "select t from Team t left join t.members m on t.name = :name";
+//            String query = "select t from Team t left join t.members m on t.username = :username";
+//            String query = "select t, m from Team t left join t.members m on t.name = :name";
+//            String query = "select t, m from Team t left join t.members m on m.username = :username";
+//            String query = "select m from Member m left join m.team t on m.name = :name";
+//            String query = "select m from Member m left join m.team t on m.username = :username";
+            String query = "select m, t from Member m left join m.team t on t.name = :name";
+//            String query = "select m, t from Member m left join m.team t on m.username = :username";
+            List resultList = em.createQuery(query)
+                    .setParameter("name", "team1")
+//                    .setParameter("username", "member1")
                     .getResultList();
 
             System.out.println("resultList.size() = " + resultList.size());
-            for (Object[] objects : resultList) {
-                for (Object object : objects) {
-                    System.out.println("object = " + object);
+            for (Object object : resultList) {
+                if (object instanceof Object[]) {
+                    new Generic<>().print((Object[]) object);
+                }
+                if (object instanceof Member) {
+                    new Generic<>().print((Member) object);
+                }
+                if (object instanceof Team) {
+                    new Generic<>().print((Team) object);
                 }
             }
-
-//            String query = "select t from Member m left outer join m.team t";
-//            List<Team> teams = em.createQuery(query, Team.class)
-//                    .getResultList();
-//            for (Team t : teams) {
-//                System.out.println("t = " + t);
-//            }
 
             tx.commit();
         } catch (Exception e) {
@@ -63,5 +82,27 @@ public class JpaMain {
             em.close();
         }
         emf.close();
+    }
+
+    static class Generic<T> {
+        public void print(T[] results) {
+            System.out.println("=====================");
+            for (T result : results) {
+                if (result != null) {
+                    System.out.println(result.getClass().getName() + "\t: result = " + result);
+                    continue;
+                }
+                System.out.println("result = " + result);
+            }
+        }
+
+        public void print(T result) {
+            System.out.println("=====================");
+            if (result != null) {
+                System.out.println(result.getClass().getName() + "\t, result = " + result);
+                return;
+            }
+            System.out.println("result = " + result);
+        }
     }
 }
