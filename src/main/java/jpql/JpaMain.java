10e8308428905fbb79faa063f9f5aa008f8a38cd
mitsui3point <rladbsgh0331@gmail.com>
2022-12-06 19:47:18 +0900

    feat($jpaMain): update - update 페치 조인의 특징과 한계
    컬렉션을 페치 조인하면 페이징 API(setFirstResult, setMaxResults)를 사용할 수 없다.
    • 일대일, 다대일 같은 단일 값 연관 필드들은 페치 조인해도 페이징 가능
    • 하이버네이트는 경고 로그를 남기고 메모리에서 페이징(매우 위험)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -47,44 +47,20 @@ public class JpaMain {
 
             em.flush();
             em.clear();
-
-            /**
-             * select t from Team t left outer join fetch t.members m
-             *
-             * - fetch join 은 나랑 연관된 모든 것들을 다 끌고오겠다는 것.
-             *
-             * 		물론 중간에 몇개를 걸러서 가져오고 싶을 수 있다.
-             * 		하지만 그런경우에서는 fetch join을 사용하면 안된다.
-             * 	ex)
-             * 		team1 과 연관된 회원이 5명 인데,
-             * 		위에 쿼리에 where m.age > 10 의 조건절을 넣었다고 하자.
-             * 		그 경우 조회되는 회원이 1명이라고 한다면,
-             * 		나머지 4명의 회원이 누락이 된다.
-             * 		예를 들어 해당 연관관계에 cascade 같은 옵션이 걸려있을 경우,
-             * 		데이터 정합성에 문제가 생기게 되어
-             * 		애플리케이션이 기대하지 않은 동작을 수행할 수 있다.
-             *
-             * 	또한 객체 그래프 탐색의 의도된 목적은 t.members 를 하게 되었을 경우
-             * 	team 한건의 엔티티에 속한 members 를 전부 가져오는 것이지,
-             * 	team 한건의 엔티티에 속한 members 를 필터링하여 가져오는것이 아님.
-             *
-             * 	또한 둘 이상의 컬렉션은 페치 조인 할 수 없다.
-             *
-             * 		일대다 관계도 정합성에 문제가 생기는데,
-             * 		일대다 일대다를 나열하듯 fetch join 을 사용하게 될 경우 겉잡을수 없다.
-             */
             String query = "select t " +
                     "from Team t " +
-                    "left outer join fetch t.members m ";
-//                    "where m.username = :username";//fetch join 에서 alias 를 사용한 조건절 제어, fetch join 에서 하면 안됨.
+                    "join fetch t.members m ";
+            //하이버네이트는 경고 로그를 남기고 메모리에서 페이징(매우 위험)
+            //WARN: HHH000104: firstResult/maxResults specified with collection fetch; applying in memory!
             List<Team> teams = em.createQuery(query, Team.class)
-//                    .setParameter("username", "회원3")
+                    .setFirstResult(0)
+                    .setMaxResults(1)
                     .getResultList();
 
             System.out.println("resultList.size() = " + teams.size());
             for (Team team : teams) {
                 new ObjectPrinter(team).print();
-                new ObjectPrinter(team.getMembers()).print();//페치 조인으로 팀과 회원을 함께 조회해서 지연 로딩 발생 안함
+                new ObjectPrinter(team.getMembers()).print();
             }
             tx.commit();
         } catch (Exception e) {
