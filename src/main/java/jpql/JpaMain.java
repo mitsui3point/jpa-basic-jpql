ec49692c6039d8eb2f29965f65cfe97fb94e67a3
mitsui3point <rladbsgh0331@gmail.com>
2022-12-06 22:50:38 +0900

    feat($jpaMain): update - update 벌크 연산 주의
    벌크 연산은 영속성 컨텍스트를 무시하고 데이터베이스에 직접 쿼리
    벌크 연산이 적용된 데이터 조회 방법
    1. 벌크 연산을 먼저 실행
    2. 벌크 연산 수행 후 영속성 컨텍스트 초기화

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -16,22 +16,33 @@ public class JpaMain {
         tx.begin();
 
         try {
-            for (int i = 0; i < 15; i++) {
-                Product product = Product.createProduct("product" + i, 1000 + (100 * i), i);
-                em.persist(product);
-            }
+            Product product1 = Product.createProduct("product1", 1000 + (100 * 1), 1);
+            em.persist(product1);
+            Product product2 = Product.createProduct("product2", 1000 + (100 * 2), 2);
+            em.persist(product2);
+            Product product3 = Product.createProduct("product3", 1000 + (100 * 3), 3);
+            em.persist(product3);
 
-            em.flush();
-            em.clear();
-
+            //FLUSH 자동 호출: commit or query 호출
             String updateQuery = "update Product p set p.price = p.price * 1.1 where p.stockAmount < 10 ";
             int updateCount = em.createQuery(updateQuery)
                     .executeUpdate();
             System.out.println("updateCount = " + updateCount);
+            System.out.println("product2 = " + product2);
+
+            /**
+             * updateQuery 가 DB에만 반영되어 있기 때문에,
+             * em.find(), em.createQuery().getResultList() 로 재조회해도 기존 값이 남아 있다.
+             * 영속성 컨텍스트를 초기화(clear) 해 주어야 조회시 벌크연산이 완료된 DB 값이 조회되고,? 1차캐시에 재등록된다.
+             *
+             * 추후 spring data jpa 에서는
+             * 이런 방식으로 제공한다. https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.modifying-queries
+             */
+            em.clear();
 
             List<Product> products = em.createQuery("select p from Product p ", Product.class).getResultList();
-            for (Product product : products) {
-                new ObjectPrinter(product).print();
+            for (Product p : products) {
+                new ObjectPrinter(p).print();
             }
 
             tx.commit();
