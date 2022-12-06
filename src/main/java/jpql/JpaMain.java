1fa1c152f7064e80da926f7bd6fe6c305619437a
mitsui3point <rladbsgh0331@gmail.com>
2022-12-06 21:07:52 +0900

    feat($jpaMain): update - update 엔티티 직접 사용 - 외래 키 값
    Team team = em.find(Team.class, 1L);
    String qlString = “select m from Member m where m.team = :team”;
    List resultList = em.createQuery(qlString)
     .setParameter("team", team)
     .getResultList();
    
    select m.* from Member m where m.team_id=?
    String qlString = “select m from Member m where m.team.id = :teamId”;
    List resultList = em.createQuery(qlString)
     .setParameter("teamId", teamId)
     .getResultList();
    
    실행된 SQL
    select m.* from Member m where m.team_id=?

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -25,9 +25,15 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team1 = Team.createTeam("team1");
+            em.persist(team1);
+
             Member member1 = Member.createMember("name1", 10, ADMIN);
+            member1.changeTeam(team1);
             em.persist(member1);
+
             Member member2 = Member.createMember("name2", 10, ADMIN);
+//            member2.changeTeam(team1);
             em.persist(member2);
 
             em.flush();
@@ -35,9 +41,9 @@ public class JpaMain {
 
             String query = "select m " +
                     "from Member m " +
-                    "where m = :member ";
+                    "where m.team = :team ";
             List members = em.createQuery(query)
-                    .setParameter("member", member1)
+                    .setParameter("team", team1)
                     .getResultList();
 
             for (Object m : members) {
