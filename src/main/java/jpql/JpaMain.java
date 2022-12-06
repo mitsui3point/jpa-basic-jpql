9324a788dd48e85049d62c28e5801d969c86032d
mitsui3point <rladbsgh0331@gmail.com>
2022-12-06 19:25:31 +0900

    feat($jpaMain): update - update 페치 조인의 특징과 한계
    페치 조인 대상에는 별칭을 줄 수 없다.
    • 하이버네이트는 가능, 가급적 사용X
    • 둘 이상의 컬렉션은 페치 조인 할 수 없다.

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -10,6 +10,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
 
+import static jpql.enumulate.MemberType.ADMIN;
 import static jpql.enumulate.MemberType.USER;
 
 public class JpaMain {
@@ -47,10 +48,37 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select distinct t " +
+            /**
+             * select t from Team t left outer join fetch t.members m
+             *
+             * - fetch join 은 나랑 연관된 모든 것들을 다 끌고오겠다는 것.
+             *
+             * 		물론 중간에 몇개를 걸러서 가져오고 싶을 수 있다.
+             * 		하지만 그런경우에서는 fetch join을 사용하면 안된다.
+             * 	ex)
+             * 		team1 과 연관된 회원이 5명 인데,
+             * 		위에 쿼리에 where m.age > 10 의 조건절을 넣었다고 하자.
+             * 		그 경우 조회되는 회원이 1명이라고 한다면,
+             * 		나머지 4명의 회원이 누락이 된다.
+             * 		예를 들어 해당 연관관계에 cascade 같은 옵션이 걸려있을 경우,
+             * 		데이터 정합성에 문제가 생기게 되어
+             * 		애플리케이션이 기대하지 않은 동작을 수행할 수 있다.
+             *
+             * 	또한 객체 그래프 탐색의 의도된 목적은 t.members 를 하게 되었을 경우
+             * 	team 한건의 엔티티에 속한 members 를 전부 가져오는 것이지,
+             * 	team 한건의 엔티티에 속한 members 를 필터링하여 가져오는것이 아님.
+             *
+             * 	또한 둘 이상의 컬렉션은 페치 조인 할 수 없다.
+             *
+             * 		일대다 관계도 정합성에 문제가 생기는데,
+             * 		일대다 일대다를 나열하듯 fetch join 을 사용하게 될 경우 겉잡을수 없다.
+             */
+            String query = "select t " +
                     "from Team t " +
-                    "left outer join fetch t.members m ";//fetch join 적용
+                    "left outer join fetch t.members m ";
+//                    "where m.username = :username";//fetch join 에서 alias 를 사용한 조건절 제어, fetch join 에서 하면 안됨.
             List<Team> teams = em.createQuery(query, Team.class)
+//                    .setParameter("username", "회원3")
                     .getResultList();
 
             System.out.println("resultList.size() = " + teams.size());
